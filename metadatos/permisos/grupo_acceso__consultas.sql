f3087da5b5c827191cc6f942e15cd456e76c2a2d
sapazos <sapazos@gmail.com>
2018-10-17 14:29:09 -0300

    reporte de actividades

---
+++ metadatos/permisos/grupo_acceso__consultas.sql
@@ -0,0 +1,64 @@
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc
+------------------------------------------------------------
+INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	'consultas', --nombre
+	NULL, --nivel_acceso
+	NULL, --descripcion
+	NULL, --vencimiento
+	NULL, --dias
+	NULL, --hora_entrada
+	NULL, --hora_salida
+	NULL, --listar
+	'0', --permite_edicion
+	NULL  --menu_usuario
+);
+
+------------------------------------------------------------
+-- apex_usuario_grupo_acc_item
+------------------------------------------------------------
+
+--- INICIO Grupo de desarrollo 0
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'1'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'2'  --item
+);
+--- FIN Grupo de desarrollo 0
+
+--- INICIO Grupo de desarrollo 280
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000147'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000148'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000172'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'extension', --proyecto
+	'consultas', --usuario_grupo_acc
+	NULL, --item_id
+	'280000180'  --item
+);
+--- FIN Grupo de desarrollo 280
